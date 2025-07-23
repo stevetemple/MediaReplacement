2069934941f5a8828589316674773184d5eec506
Steve Temple <steve@gibedigital.com>
2025-07-23 10:32:03 +0100

    Make v1 match talk state

--- src/MediaReplacement.Module/src/Repository/my-media-tree.store.context-token.ts
+++
@@ -1,4 +0,0 @@
-import type MyMediaTreeStore from './my-media-tree.store';
-import { UmbContextToken } from '@umbraco-cms/backoffice/context-api';
-
-export const MY_MEDIA_TREE_STORE_CONTEXT = new UmbContextToken<MyMediaTreeStore>('MyMediaTreeStore');
--- src/MediaReplacement.Module/src/manifests.ts
+++ src/MediaReplacement.Module/src/manifests.ts
@@ -1,8 +1,5 @@
 
 import { umbExtensionsRegistry as e } from "@umbraco-cms/backoffice/extension-registry";
-import MyMediaTreeRepository from './Repository/my-media-tree-repository.js';
-import MyMediaTreeStore from './Repository/my-media-tree.store.js';
-import { ContentMediaRespository } from "./ContentMedia/content-media-repository.js";
 
 e.unregister("Umb.Modal.MediaPicker");
 const o = [
@@ -12,30 +9,6 @@ const o = [
     type: "modal",
     js: () => import('./media-picker-replacement.js') 
   },
-  {
-    type: 'repository',
-    alias: 'My.Media.Tree.Repository',
-    name: 'My Media Tree Repository',
-    api: MyMediaTreeRepository
-  },
-  {
-    type: 'treeStore',
-    alias: 'My.Media.Tree.Store',
-    name: 'My Media Tree Store',
-    api: MyMediaTreeStore
-  },
-  {
-    type: 'repository',
-    alias: 'ContentMediaRepository',
-    name: 'Content Media Repository',
-    api: ContentMediaRespository
-  },
-  {
-    type: 'element',
-    alias: 'Media.Folder.Thumbnails',
-    name: "Media folder thumbnails element",
-    js: () => import('./media-folder-thumbnails.element.js')
-  }
 ], a = [
   ...o
 ];
